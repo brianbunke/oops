0162c22493ee9ab0d575873f0a2da1d3b7ad773a
Brian Bunke <me@brianbunke.com>
2018-03-28 20:57:49 -0700

    Add first test file

---
+++ Tests/oops.Tests.ps1
@@ -0,0 +1,17 @@
+﻿Remove-Module DemoModule -ErrorAction SilentlyContinue -Force
+Import-Module $PSScriptRoot\..\DemoModule.psd1
+
+Describe 'Test DemoModule' {
+    $GCM = Get-Command -Module DemoModule
+
+    It 'Contains two commands' {
+        # This is a test of a test of a test, my friends
+        $GCM.Count | Should -Be 2
+    }
+
+    ForEach ($Command in $GCM) {
+        Get-ModuleHelp $Command
+
+        $Command | Get-Parameter | Compare-Parameter C:\Users\brian.bunke\Desktop\param.json
+    }
+}
