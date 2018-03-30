1675ed60015811aae5cfe74cdb08161bbc5c698d
Brian Bunke <me@brianbunke.com>
2018-03-29 21:11:26 -0700

    Polish Export-Parameter

--- oops/Public/Export-Parameter.ps1
+++ oops/Public/Export-Parameter.ps1
@@ -1,38 +1,41 @@
-﻿# TODO: Change name
-    # Hardcoded output
-    # Param name ok?
-    # Param type ok?
-    # Write function description
-    # Redo description
-    # Redo example
-
-function Export-Parameter {
+﻿function Export-Parameter {
     <#
     .SYNOPSIS
-    Compares stored state of a command's parameters against actual state.
+    Export parameter details of the supplied command(s) to a JSON file.
 
     .DESCRIPTION
-    Runs Pester tests against a command to check for common issues, like:
-    - Help contains a Synopsis and Description
-    - Help contains example code and explanations
-    - Help exists for all non-default parameters
-    - Help does not list non-existent parameters
+    Records command details in a param.json file. By storing this JSON with your
+    module tests, you persist a previous, known-good record of command parameters.
+
+    Export-Parameter expects an output folder, and currently always creates a
+    file named `param.json`. A -Force parameter is available for file overwrite.
+    
+    Intended to intake piped output from Get-Parameter.
 
     .EXAMPLE
-    Get-ModuleHelp Get-Date
-    Runs Pester tests to ensure the help for Get-Date isn't missing anything obvious.
+    Get-Command -Module oops | Get-Parameter | Export-Parameter
+    Collects command and parameter info for module oops.
+    Converts data to JSON and saves in `param.json` at the current location.
+    
+    .EXAMPLE
+    Get-Command -Module oops | Get-Parameter | Export-Parameter -OutputFolder C:\
+    Collects command and parameter info for module oops.
+    Converts data to JSON and saves in `param.json` at the root of the C:\ drive.
     #>
-    [CmdletBinding()]
+    [CmdletBinding(
+        SupportsShouldProcess = $true,
+        ConfirmImpact = 'Low')]
     param (
+        # Select a folder to create a new DemoModule.json file inside
+        [ValidateScript({Test-Path $_ -PathType Container})]
+        $OutputFolder = (Get-Location),
+
+        # Accepts command/parameter object output from Get-Parameter
         [Parameter(
             Mandatory = $true,
             ValueFromPipeline = $true
         )]
-        [PSCustomObject[]]$Command,
-
-        # Select a folder to create a new DemoModule.json file inside
-        [ValidateScript({Test-Path $_ -PathType Container})]
-        $OutputFolder = 'C:\Users\brian.bunke\Desktop'
+        [PSCustomObject[]]$CmdParam
     )
 
     BEGIN {
@@ -41,20 +44,26 @@ function Export-Parameter {
     }
 
     PROCESS {
-        ForEach ($c in $Command) {
+        ForEach ($c in $CmdParam) {
             # Append the command into $List
             [void]$List.Add($c)
         }
     }
 
     END {
-        # Output commands, parameters, and notable attributes in JSON
-        Try {
-            # Just needs depth 3 for sure, but ¯\_(ツ)_/¯
-            Write-Verbose "Exporting state to $OutputFolder\param.json"
-            $List | ConvertTo-Json -Depth 5 | Out-File $OutputFolder\param.json -ErrorAction Stop
-        } Catch {
-            throw "Unable to create '$OutputFolder\param.json'. Do you need admin rights?"
-        }
-    }
+        # WhatIf
+        If ($PSCmdlet.ShouldProcess(
+            "$OutputFolder\param.json",
+            "Record state of commands and parameters"
+        )) {
+            # Output commands, parameters, and notable attributes to JSON file
+            Try {
+                Write-Verbose "Exporting state to $OutputFolder\param.json"
+                # Just needs depth 3 for sure, but ¯\_(ツ)_/¯
+                $List | ConvertTo-Json -Depth 5 | Out-File $OutputFolder\param.json -ErrorAction Stop
+            } Catch {
+                throw "Unable to create '$OutputFolder\param.json'. Do you need admin rights?"
+            }
+        } #WhatIf
+    } #END
 }
