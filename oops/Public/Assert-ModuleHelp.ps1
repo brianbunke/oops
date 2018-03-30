d8dca30d7bddb7d0d5d3e8669c72d97675c6448f
Brian Bunke <me@brianbunke.com>
2018-03-29 22:49:03 -0700

    Polish Assert-ModuleHelp

--- oops/Public/Assert-ModuleHelp.ps1
+++ oops/Public/Assert-ModuleHelp.ps1
@@ -1,24 +1,22 @@
-﻿# TODO: Change name
-    # Param name ok?
-    # Param type ok?
-    # Write function description
-    # Redo example
-
-function Assert-ModuleHelp {
+﻿function Assert-ModuleHelp {
     <#
     .SYNOPSIS
     Checks for common command help issues for the supplied command.
 
     .DESCRIPTION
-    Runs Pester tests against a command to check for common issues, like:
+    Runs Pester tests against supplied command(s) to check for common issues:
     - Help contains a Synopsis and Description
     - Help contains example code and explanations
     - Help exists for all non-default parameters
     - Help does not list non-existent parameters
 
     .EXAMPLE
-    Get-ModuleHelp Get-Date
+    Get-Command Get-Date | Assert-ModuleHelp
     Runs Pester tests to ensure the help for Get-Date isn't missing anything obvious.
+
+    .EXAMPLE
+    Get-Command -Module oops | Assert-ModuleHelp -Verbose
+    Perform help coverage tests against all commands in the oops module.
 
     .NOTES
     All of the thanks to June Blender (@juneb_get_help) for the precursor to this work,
@@ -27,67 +25,96 @@ function Assert-ModuleHelp {
     #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $true)]
-        $Command
+        # Command(s) to extract relevant info from
+        # Most easily supplied by piping in Get-Command
+        [Parameter(
+            Mandatory = $true,
+            ValueFromPipeline = $true
+        )]
+        [System.Management.Automation.CommandInfo[]]$Command
     )
 
-    # Record the MamlCommandHelpInfo and FunctionInfo for the command
-    $GetHelp     = Get-Help $Command
-    $GetCommand  = Get-Command $Command
-
-    If (-not $GetHelp -or -not $GetCommand) {
-        throw "Unable to Get-Help or Get-Command against $Command"
+    BEGIN {
+        # Create an empty list to hold all commands
+        $List = New-Object System.Collections.Generic.List[Object]
     }
 
-    $CommandName = $GetCommand.Name
+    PROCESS {
+        ForEach ($GetCommand in $Command) {
+            $Name    = $GetCommand.Name
+            Write-Verbose "Processing $Name"
 
-    Write-Verbose "Processing $CommandName"
+            $GetHelp = Get-Help $Name
 
-    # Record all examples in the command help
-    $ExampleList = $GetHelp.Examples.Example
+            # Get a list of all non-default parameters in the command
+            $CmdParam = Get-CommandParameter $GetCommand
 
-    # Get a list of all non-default parameters in the command
-    $GCMParameterList = Get-CommandParameter $GetCommand
+            # Get a list of all non-default parameters in the help
+            $HelpParam = $GetHelp.Parameters.Parameter |
+                Where-Object Name -notin (Get-DefaultParameter)
 
-    Describe "$CommandName help coverage" {
-        It "Synopsis should not be empty" {
-            $GetHelp.Synopsis | Should -Not -BeNullOrEmpty
-        }
+            # Record all examples in the command help
+            $ExampleList = $GetHelp.Examples.Example
 
-        It "Synopsis should not be auto-generated" {
-            $GetHelp.Synopsis | Should -Not -BeLike '*`[<CommonParameters>`]*'
-        }
-
-        It "Description should not be empty" {
-            $GetHelp.Description | Should -Not -BeNullOrEmpty
-        }
-
-        It "Contains at least one example" {
-            ($ExampleList | Select-Object -First 1).Code | Should -Not -BeNullOrEmpty
-        }
-
-        # Loop through all examples
-        for ($i = 0; $i -lt $ExampleList.Count; $i++) {
-            It "Explains Example $($i+1)" {
-                $ExampleList[$i].Remarks.Text | Should -Not -BeNullOrEmpty
+            $cmd = [PSCustomObject]@{
+                Name        = $Name
+                Help        = $GetHelp
+                Command     = $GetCommand
+                CmdParam    = $CmdParam
+                HelpParam   = $HelpParam
+                ExampleList = $ExampleList
             }
-        }
 
-        If ($GCMParameterList) {
-            ForEach ($Parameter in $GCMParameterList) {
-                $pName = $Parameter.Name
-                $pHelp = $GetHelp.Parameters.Parameter | Where-Object Name -EQ $pName
+            # Append the command into $List
+            [void]$List.Add($cmd)
+        } #ForEach $c
+    }
 
-                It "-$pName parameter contains help" {
-                    $pHelp.Description.Text | Should -Not -BeNullOrEmpty
-                }
-
-                ForEach ($helpParam in $pHelp.Name) {
-                    It "-$helpParam parameter, found in help, still exists in code" {
-                        $helpParam -in $GCMParameterList.Name | Should -Be $true
+    END {
+        Describe 'Ensure commands contain proper help' {
+            ForEach ($cmd in $List) {
+                Context "$($cmd.Name) help coverage" {
+                    It 'Synopsis should not be auto-generated' {
+                        $cmd.Help.Synopsis | Should -Not -BeLike '*`[<CommonParameters>`]*'
                     }
-                }
-            } #ForEach $Parameter
-        } #If $ParameterList
-    } #Describe
+
+                    It 'Synopsis should not be empty' {
+                        $cmd.Help.Synopsis | Should -Not -BeNullOrEmpty
+                    }
+
+                    It 'Description should not be empty' {
+                        $cmd.Help.Description | Should -Not -BeNullOrEmpty
+                    }
+
+                    It 'Contains at least one example' {
+                        ($cmd.ExampleList | Select-Object -First 1).Code | Should -Not -BeNullOrEmpty
+                    }
+
+                    # Loop through all examples
+                    for ($i = 0; $i -lt $cmd.ExampleList.Count; $i++) {
+                        It "Explains Example $($i+1)" {
+                            $cmd.ExampleList[$i].Remarks.Text[0] | Should -Not -BeNullOrEmpty
+                        }
+                    }
+
+                    If ($cmd.CmdParam) {
+                        ForEach ($Parameter in $cmd.CmdParam) {
+                            $pName = $Parameter.Name
+                            $pHelp = $cmd.HelpParam | Where-Object Name -eq $pName
+
+                            It "-$pName parameter contains help" {
+                                $pHelp.Description.Text | Should -Not -BeNullOrEmpty
+                            }
+                        } #ForEach $Parameter
+                    } #If $cmd.ParamList
+
+                    It 'Parameters found in help still exist in code' {
+                        $cmd.HelpParam.Name | ForEach-Object {
+                            $_ -in $cmd.CmdParam.Name | Should -Be $true
+                        }
+                    }
+                } #Context
+            } #ForEach $cmd
+        } #Describe
+    } #END
 }
