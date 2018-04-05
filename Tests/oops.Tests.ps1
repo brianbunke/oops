90a5463b85017bc0304a9ac8cc097752814d7763
Brian Bunke <me@brianbunke.com>
2018-04-05 15:59:00 -0700

    Add PSScriptAnalyzer example

--- Tests/oops.Tests.ps1
+++ Tests/oops.Tests.ps1
@@ -1,13 +1,30 @@
-﻿Remove-Module oops -ErrorAction SilentlyContinue -Force
+﻿#Requires -Modules PSScriptAnalyzer
+
+Remove-Module oops -ErrorAction SilentlyContinue -Force
 Import-Module $PSScriptRoot\..\oops\oops.psd1
 
 Describe 'Module-level oops tests' {
     $GCM = Get-Command -Module oops
 
     # A basic test before getting meta
-    It 'Contains four commands' {
+    It 'Contains four public commands' {
         $GCM.Count | Should -Be 4
     }
+
+    # Call PSScriptAnalyzer with the default rule set
+    Describe 'Ensure commands pass default PSScriptAnalyzer rules' {
+        $ViolationSet = Invoke-ScriptAnalyzer -Path $PSScriptRoot\..\oops\ -Recurse
+        $RuleSet      = Get-ScriptAnalyzerRule
+
+        ForEach ($Rule in $RuleSet) {
+            It "Should pass $Rule" {
+                If (($ViolationSet) -and ($ViolationSet.RuleName -contains $Rule)) {
+                    $ViolationSet | Where-Object RuleName -eq $Rule -OutVariable Failures | Out-Default
+                    $Failures.Count | Should -Be 0
+                }
+            }
+        }
+    } #Describe PSScriptAnalyzer
 
     # Ensure each command has proper help coverage
     $GCM | Assert-ModuleHelp
