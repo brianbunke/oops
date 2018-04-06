b2257a9aae2b4f5ffe6e770ce4501673cde93718
Brian Bunke <me@brianbunke.com>
2018-04-05 21:08:41 -0700

    Modify PSScriptAnalyzer output

--- Tests/oops.Tests.ps1
+++ Tests/oops.Tests.ps1
@@ -6,7 +6,7 @@ Import-Module $PSScriptRoot\..\oops\oops.psd1
 Describe 'Module-level oops tests' {
     $GCM = Get-Command -Module oops
 
-    # A basic test before getting meta
+    # A basic example assertion before getting meta
     It 'Contains four public commands' {
         $GCM.Count | Should -Be 4
     }
@@ -19,11 +19,15 @@ Describe 'Module-level oops tests' {
         ForEach ($Rule in $RuleSet) {
             It "Should pass $Rule" {
                 If (($ViolationSet) -and ($ViolationSet.RuleName -contains $Rule)) {
-                    $ViolationSet | Where-Object RuleName -eq $Rule -OutVariable Failures | Out-Default
+                    $ViolationSet |
+                        Where-Object RuleName -eq $Rule -OutVariable Failures |
+                        Select-Object Line, ScriptName, Severity, Message |
+                        Out-Default
+
                     $Failures.Count | Should -Be 0
                 }
-            }
-        }
+            } #It
+        } #ForEach $RuleSet
     } #Describe PSScriptAnalyzer
 
     # Ensure each command has proper help coverage
